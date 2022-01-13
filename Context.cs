2d96e8f83575ad7436194127a7c685899890e844
AndrewVGoncalves <72450101+AndrewVGoncalves@users.noreply.github.com>
2022-01-13 19:21:55 -0300

    primeiro commit

---
+++ Context.cs
@@ -0,0 +1,33 @@
+﻿using TrabalhoFinalAndrew.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrabalhoFinalAndrew
+{
+    public class Context : DbContext
+    {
+        public DbSet<Album> Album { get; set; }
+
+        public DbSet<Fotos> Fotos { get; set; }
+        public DbSet<Pessoa> Pessoa { get; set; }
+
+        public DbSet<Pessoa_Fotos> Pessoas_Fotos { get; set; }
+
+        public Context(DbContextOptions<Context> opt) : base(opt)
+        {
+        }
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /*modelBuilder
+                .Entity<Pessoas_Fotos>()
+                .HasKey(f => f.Fotos)
+                .HasKey(p => p.Pessoas)
+                .On
+                /*.HasAlternateKey(x => x.Fotos)
+                .HasOne(e => e.Pessoa)
+                .WithMany(e => e.Fotos)
+                .OnUpdate(up)
+                .OnDelete(DeleteBehavior.ClientCascade);*/
+
+        internal void SavedChanges() => throw new NotImplementedException();
+}
+}
